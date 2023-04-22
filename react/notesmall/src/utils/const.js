e335b40fcd7be959e114e6aff96e86071f4af147
林晉守 <orin@linjinshoudeMacBook-Air.local>
2023-04-22 17:28:53 +0800

    [update] react api update

--- react/notesmall/src/utils/const.js
+++ react/notesmall/src/utils/const.js
@@ -1,2 +1,2 @@
-export const graphqlAPI = "http://localhost:8000/graphql"
-export const imageAPI = "http://localhost:8000/"
+export const graphqlAPI = `${process.env.REACT_APP_API_URL}graphql`
+export const imageAPI = process.env.REACT_APP_API_URL
