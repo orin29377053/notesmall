e0577b07d45e49d9390b866e8e0b7d4afc2fbe19
林晉守 <orin@linjinshoudeMacBook-Air.local>
2023-04-22 15:47:50 +0800

    [test] githubaction test2

--- react/notesmall/src/components/layout/Home.jsx
+++ react/notesmall/src/components/layout/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home2</div>
-  )
-}
+    return <div>Home2{process.env.REACT_APP_TEST}</div>;
+};
 
-export default Home
+export default Home;
