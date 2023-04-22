dc1060d4b7824c0d8fd1d63337c1e90f2ad3a585
林晉守 <orin@linjinshoudeMacBook-Air.local>
2023-04-22 17:11:56 +0800

    [update] redis config update

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
 const Keyv = require("keyv");
-const cache=new Keyv("redis://default@127.0.0.1:6379")
+const cache = new Keyv(process.env.REDIS);
+cache.set("foo", "bar");
 exports.cache=cache
